de4464913eec96aeba3ff3f1d6ad9b5910ca639d
Egor Panevin <predsedatel228@yandex.ru>
2021-06-06 14:20:05 +0300

    сделаны правки для 2itr

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,29 @@
 let editButton = document.querySelector('.profile-info__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseIcon = document.querySelector('.popup__close-icon');
-let popupSaveButton = document.querySelector('.popup__save-button');
-let popupName = document.querySelector('.popup__name');
-let popupDescription = document.querySelector('.popup__description');
+let popupSaveButton = document.querySelector('.form__save-button');
+let formName = document.querySelector('.form__info_type_name');
+let formDescription = document.querySelector('.form__info_type_description');
 let ProfileInfoTitle = document.querySelector('.profile-info__title');
 let ProfileInfoSubtitle = document.querySelector('.profile-info__subtitle');
+let form = document.querySelector('.form');
 
 function togglePopup() {
   popup.classList.toggle('popup_opened');
 }
-
+function copyToForm () {
+  formName.value = ProfileInfoTitle.textContent;
+  formDescription.value = ProfileInfoSubtitle.textContent;
+  togglePopup();
+}
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  ProfileInfoTitle.textContent = popupName.value;
-  ProfileInfoSubtitle.textContent = popupDescription.value;
+  ProfileInfoTitle.textContent = formName.value;
+  ProfileInfoSubtitle.textContent = formDescription.value;
   togglePopup();
 }
 
-editButton.addEventListener('click', togglePopup);
+editButton.addEventListener('click', copyToForm);
 popupCloseIcon.addEventListener('click', togglePopup);
-popupSaveButton.addEventListener('click', formSubmitHandler); 
+form.addEventListener('submit', formSubmitHandler); 
+
